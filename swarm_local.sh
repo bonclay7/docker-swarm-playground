219b79da4104776f10f325fc913e11236ba2b661
Florent Ramière <florent@ramiere.com>
2016-02-17 17:29:07 +0100

    add consul registrator

--- swarm_local.sh
+++ swarm_local.sh
@@ -6,9 +6,8 @@ docker-machine ip registry || {
 	echo "Creating local docker registry machine"
 	docker-machine create -d virtualbox registry
 
-	eval $(docker-machine env registry)
-
-	docker run -d -p 5000:5000 --name registry-mirror \
+	echo "Launching docker registry cache service"
+	docker $(docker-machine config registry) run -d -p 5000:5000 --name registry-mirror \
  	   -v $(pwd)/registry/cache:/var/lib/registry/ \
 		 -v $(pwd)/registry/configuration/:/etc/registry/ \
 		 registry:2 /etc/registry/config.yml	
@@ -22,11 +21,15 @@ docker-machine ip consul || {
 			--engine-insecure-registry registry-1.docker.io \
 			consul
 
+	echo "Launching consul machine"
 	docker $(docker-machine config consul) run -d \
 	    -p "8500:8500" \
 	    -h "consul" \
 	    progrium/consul -server -bootstrap
 }
+
+echo "Removing old cluster"
+docker-machine rm -f swarm-master swarm-node1 swarm-node2
 
 docker-machine create \
     -d virtualbox \
@@ -39,6 +42,7 @@ docker-machine create \
     --engine-opt="cluster-advertise=eth1:0" \
     swarm-master
 
+docker-machine ip node1 || docker-machine rm node1
 docker-machine create \
     -d virtualbox \
 		--engine-registry-mirror http://$(docker-machine ip registry):5000 \
@@ -49,6 +53,7 @@ docker-machine create \
     --engine-opt="cluster-advertise=eth1:0" \
     swarm-node1
 
+docker-machine ip node2 || docker-machine rm node2
 docker-machine create \
     -d virtualbox \
 		--engine-registry-mirror http://$(docker-machine ip registry):5000 \
@@ -59,8 +64,39 @@ docker-machine create \
     --engine-opt="cluster-advertise=eth1:0" \
     swarm-node2
 
+eval $(docker-machine env swarm-master)
+docker rm -f registrator
+docker run -d \
+    --name=registrator \
+    --net=host \
+    --volume=/var/run/docker.sock:/tmp/docker.sock \
+    gliderlabs/registrator:latest \
+    consul://$(docker-machine ip consul):8500
+
+eval $(docker-machine env swarm-node1)
+docker rm -f registrator
+docker run -d \
+    --name=registrator \
+    --net=host \
+    --volume=/var/run/docker.sock:/tmp/docker.sock \
+    gliderlabs/registrator:latest \
+    consul://$(docker-machine ip consul):8500
+
+eval $(docker-machine env swarm-node2)
+docker rm -f registrator
+docker run -d \
+    --name=registrator \
+    --net=host \
+    --volume=/var/run/docker.sock:/tmp/docker.sock \
+    gliderlabs/registrator:latest \
+    consul://$(docker-machine ip consul):8500
+
+
 eval $(docker-machine env --swarm swarm-master)
 docker network create --driver overlay bowwow-net
+
+
+docker run -itd nginx
 
 docker run -itd --name=webtest --net=bowwow-net --env="constraint:node==swarm-node1" nginx
 docker run -it --net=bowwow-net --env="constraint:node==swarm-node2" busybox wget -O- http://webtest
