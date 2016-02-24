4831fcc16b7812a6377f3f21c3ce61daf203458c
Florent Ramière <florent@ramiere.com>
2016-02-24 17:01:53 +0100

    Setup influxdb in chronograph

--- tick.sh
+++ tick.sh
@@ -16,3 +16,12 @@ docker exec -t $(docker ps | grep tick_kapacitor | awk '{print $1}') /etc/kapaci
 
 echo "Using grafana api for further configurations"
 docker exec -t $(docker ps | grep tick_grafana | awk '{print $1}') /etc/grafana/api-client.sh
+
+echo "Setup influxdb in chronograph"
+influxdb_container=$(docker ps | grep tick_influxdb | awk '{print $1}')
+influxdb_ip=$(docker inspect --format '{{ .Node.IP }}' $influxdb_container)
+
+chronograf_container=$(docker ps | grep tick_chronograf | awk '{print $1}')
+chronograf_ip=$(docker inspect --format '{{ .Node.IP }}' $chronograf_container)
+
+echo '{"id":1,"name":"influxdb","url":"http://'$influxdb_ip':8086"}' | curl -d @- "http://$chronograf_ip:10000/api/v0/servers"
